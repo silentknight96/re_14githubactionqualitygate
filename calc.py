c25d4b666ae0256ee86c386c0a0871314bd8481b
silentknight96 <nik219606@gmail.com>
2025-11-04 13:56:35 +0530

    testing1

--- calc.py
+++ calc.py
@@ -1,13 +1,13 @@
-def add(a, b):
+def additation(a, b):
     return a + b
 
-def sub(a, b):
+def substraction(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero.")
     return a / b
