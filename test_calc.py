58e3acb4241a0c03331f6d3887f601a6fc239c3d
silentknight96 <nik219606@gmail.com>
2025-11-04 13:59:50 +0530

    testing1

--- test_calc.py
+++ test_calc.py
@@ -1,1 +1,16 @@
 import pytest_
+
+from calc import additation, subtract, multiply, divide
+
+def test_additation ():
+    #compare actual output with expected output
+    assert add(2, 3) == 5
+
+def test_substract():
+    assert sub(3, 1) == 2
+
+def test_multiply():
+    assert multiply(4, 2) == 8
+
+def test_divide():
+    assert divide(4, 2) == 2    
